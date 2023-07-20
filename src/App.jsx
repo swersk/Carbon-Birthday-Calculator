35e135f38a9046d80c576b155fe8754e5a02c875
Lauren Swersky <lauren.swersky@gmail.com>
2023-07-20 18:13:46 -0400

    heading styling, cards are animated

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Button, Typography, MenuItem, Select, FormControl, Container, Box, Grid } from '@mui/material';
+import { Button, Typography, MenuItem, Select, FormControl, Container, Box } from '@mui/material';
 import Confetti from 'react-confetti';
-import { Spring } from 'react-spring';
 import ResultsCards from './ResultsCards';
+import { useSpring, animated } from 'react-spring';
 
 
 const App = () => {
-
 
   useEffect(() => {
     fetch('./carbondata.csv')
@@ -40,7 +39,7 @@ const App = () => {
   const [difference, setDifference] = useState(0);
   const [trend, setTrend] = useState(0)
   const [showConfetti, setShowConfetti] = useState(false);
-  const [showRain, setShowRain] = useState(false);
+  const [isBouncing, setIsBouncing] = useState(false);
 
 
   const current = new Date();
@@ -48,74 +47,73 @@ const App = () => {
   let months = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'];
 
-    let obj = {
-      1959: 21.38,
-      1960: 22.17,
-      1961: 22.43,
-      1962: 22.23,
-      1963: 22.08,
-      1964: 21.65,
-      1965: 21.43,
-      1966: 21.89,
-      1967: 21.71,
-      1968: 21.74,
-      1969: 22.63,
-      1970: 23.14,
-      1971: 23.42,
-      1972: 23.75,
-      1973: 24.93,
-      1974: 24.91,
-      1975: 25.16,
-      1976: 24.72,
-      1977: 25.73,
-      1978: 26.52,
-      1979: 27.40,
-      1980: 28.39,
-      1981: 29.16,
-      1982: 30.57,
-      1983: 32.03,
-      1984: 33.94,
-      1985: 35.32,
-      1986: 36.25,
-      1987: 37.13,
-      1988: 38.87,
-      1989: 39.45,
-      1990: 39.49,
-      1991: 39.50,
-      1992: 38.69,
-      1993: 38.05,
-      1994: 38.38,
-      1995: 38.36,
-      1996: 38.35,
-      1997: 37.06,
-      1998: 36.84,
-      1999: 37.02,
-      2000: 35.91,
-      2001: 34.05,
-      2002: 32.79,
-      2003: 33.11,
-      2004: 31.72,
-      2005: 30.53,
-      2006: 28.44,
-      2007: 27.69,
-      2008: 28.33,
-      2009: 26.28,
-      2010: 25.73,
-      2011: 22.77,
-      2012: 23.21,
-      2013: 21.62,
-      2014: 19.74,
-      2015: 17.66,
-      2016: 17.45,
-      2017: 16.14,
-      2018: 11.92,
-      2019: 11.17,
-      2020: 6.79,
-      2021: 4.23,
-      2022: 4.23,
-      2023: 4.23
-    };
-
+  let obj = {
+    1959: 21.38,
+    1960: 22.17,
+    1961: 22.43,
+    1962: 22.23,
+    1963: 22.08,
+    1964: 21.65,
+    1965: 21.43,
+    1966: 21.89,
+    1967: 21.71,
+    1968: 21.74,
+    1969: 22.63,
+    1970: 23.14,
+    1971: 23.42,
+    1972: 23.75,
+    1973: 24.93,
+    1974: 24.91,
+    1975: 25.16,
+    1976: 24.72,
+    1977: 25.73,
+    1978: 26.52,
+    1979: 27.40,
+    1980: 28.39,
+    1981: 29.16,
+    1982: 30.57,
+    1983: 32.03,
+    1984: 33.94,
+    1985: 35.32,
+    1986: 36.25,
+    1987: 37.13,
+    1988: 38.87,
+    1989: 39.45,
+    1990: 39.49,
+    1991: 39.50,
+    1992: 38.69,
+    1993: 38.05,
+    1994: 38.38,
+    1995: 38.36,
+    1996: 38.35,
+    1997: 37.06,
+    1998: 36.84,
+    1999: 37.02,
+    2000: 35.91,
+    2001: 34.05,
+    2002: 32.79,
+    2003: 33.11,
+    2004: 31.72,
+    2005: 30.53,
+    2006: 28.44,
+    2007: 27.69,
+    2008: 28.33,
+    2009: 26.28,
+    2010: 25.73,
+    2011: 22.77,
+    2012: 23.21,
+    2013: 21.62,
+    2014: 19.74,
+    2015: 17.66,
+    2016: 17.45,
+    2017: 16.14,
+    2018: 11.92,
+    2019: 11.17,
+    2020: 6.79,
+    2021: 4.23,
+    2022: 4.23,
+    2023: 4.23
+  };
 
   let startYear = 1959;
   let endYear = 2023;
@@ -172,9 +170,9 @@ const App = () => {
 
     //confetti
     setShowConfetti(true);
+    setIsBouncing(true); // Start the bouncing animation
     setTimeout(() => {
       setShowConfetti(false);
-      setShowRain(true);
     }, 8000);
 
   };
@@ -199,92 +197,130 @@ const App = () => {
     }
   }, [avg]);
 
+    // Config for the bouncing animation
+    const bouncingConfig = {
+      from: { transform: 'translateY(0)' },
+      to: { transform: 'translateY(-100px)' },
+      config: { mass: 1, tension: 100, friction: 10, duration: 30000 },
+      loop: true
+    };
+
+      // Use the useSpring hook to create the bouncing animation
+  const bouncingAnimation = useSpring(isBouncing ? bouncingConfig : {});
+
+
   return (
     <>
-    <Container>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-          minHeight: '50vh',
-        }}
-      >
-        <Typography variant="h4" sx={{ p: '8px' }}>What's your Carbon Dioxide Birth Number?</Typography>
-        <Typography variant="h5" sx={{ mb: '32px' }}>Select your birth information to find out!</Typography>
-
-        <div className="month">
-          <FormControl sx={{ m: 1, width: 300 }}>
-            <Select
-              value={month}
-              onChange={handleMonthClick}
-              displayEmpty
-              inputProps={{ 'aria-label': 'Month' }}
-              sx={{
-                fontSize: "1rem",
-                ":hover": { bgcolor: "lightgrey" },
-              }}
-            >
-              <MenuItem value="" disabled>Month</MenuItem>
-              {months.map((month) => (
-                <MenuItem key={month} value={month}>{month}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </div>
-
-        <div className="year">
-          <FormControl sx={{ m: 1, width: 300 }}>
-            <Select
-              value={year}
-              onChange={handleYearClick}
-              displayEmpty
-              inputProps={{ 'aria-label': 'Year' }}
-              sx={{
-                fontSize: "1rem",
-                mb: '16px',
-                ":hover": { bgcolor: "lightgrey" },
-              }}
-            >
-              <MenuItem value="" disabled>Year</MenuItem>
-              {years.map((year) => (
-                <MenuItem key={year} value={year}>{year}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </div>
-
-        <Button
-          variant="contained"
-          onClick={handleAvgClick}
+      <Container>
+        <Box
           sx={{
-            width: "fit-content",
-            fontSize: "1rem",
-            ":hover": { bgcolor: "lightBlue" }
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+            minHeight: '50vh',
+            border: '1px solid #ccc',
+            marginTop: '90px',
+            borderTop: '5px solid #287AB8',
+            marginLeft: '80px',
+            marginRight: '80px',
+            backgroundColor: '#F8F8F8',
           }}
         >
-          GET MY RESULTS
-        </Button>
+          <Typography variant="h4" sx={{ p: '8px'}}>What's your Carbon Dioxide Birth Number?</Typography>
+          <Typography variant="h5" sx={{ mb: '32px' }}>Select your birth information to find out!</Typography>
 
-        {showConfetti && (
-          <Confetti
+          <div className="month">
+            <FormControl sx={{ m: 1, width: 300 }}>
+              <Select
+                value={month}
+                onChange={handleMonthClick}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Month' }}
+                sx={{
+                  fontSize: "1rem",
+                  ":hover": { bgcolor: "lightgrey" },
+                }}
+              >
+                <MenuItem value="" disabled>Month</MenuItem>
+                {months.map((month) => (
+                  <MenuItem key={month} value={month}>{month}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          <div className="year">
+            <FormControl sx={{ m: 1, width: 300 }}>
+              <Select
+                value={year}
+                onChange={handleYearClick}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Year' }}
+                sx={{
+                  fontSize: "1rem",
+                  mb: '16px',
+                  ":hover": { bgcolor: "lightgrey" },
+                }}
+              >
+                <MenuItem value="" disabled>Year</MenuItem>
+                {years.map((year) => (
+                  <MenuItem key={year} value={year}>{year}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          <Button
+            variant="contained"
+            onClick={handleAvgClick}
+            sx={{
+              width: "fit-content",
+              marginTop: '20px',
+              marginBottom: '20px',
+              fontSize: "1rem",
+              ":hover": { bgcolor: "lightBlue" }
+            }}
+          >
+            GET MY RESULTS
+          </Button>
+          </Box>
+
+          {showConfetti && (
+            <Confetti
               width={window.innerWidth}
               height={window.innerHeight}
               numberOfPieces={200}
-          />
-        )}
+            />
+          )}
 
-        {avg && (
-          <>
-            <Typography  className="results" variant="h4" sx={{ textAlign: 'center', pt: '40px', pb: '30px', mt: '50px' }}>Your Results</Typography>
-            <ResultsCards month={month} year={year} avg={avg} increase={increase} difference={difference} trend={trend}/>
-            <Share setShowConfetti={setShowConfetti} />
-            <Blurb />
-          </>
-        )}
-      </Box>
-    </Container>
+
+
+          {avg && (
+            <>
+              <animated.div style={bouncingAnimation}>
+              <Typography
+                className="results"
+                variant="h4"
+                sx={{ textAlign: 'center', pt: '40px', pb: '30px', mt: '50px' }}
+                >
+                  Your Results
+                </Typography>
+              </animated.div>
+              <ResultsCards
+              month={month}
+              year={year}
+              avg={avg}
+              increase={increase}
+              difference={difference}
+              trend={trend}
+              />
+              <Share setShowConfetti={setShowConfetti} />
+              <Blurb />
+            </>
+          )}
+
+      </Container>
     </>
   );
 }
--- src/ResultsCards.jsx
+++ src/ResultsCards.jsx
@@ -1,112 +1,186 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Typography, MenuItem, Select, FormControl, Container, Box, Grid } from '@mui/material';
+import React from 'react';
+import { Typography, Box, Grid } from '@mui/material';
+import { useTrail, animated } from 'react-spring';
 
-const ResultsCards = ({month, year, avg, increase, difference, trend} ) => {
+const ResultsCards = ({ month, year, avg, increase, difference, trend }) => {
+  // Create an array of styles for each card
+  const trail = useTrail(4, {
+    from: { opacity: 0, transform: 'translateY(-20px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+  });
+
+  const cardData = [
+    {
+      title: `In ${month} ${year}, there were`,
+      value: `${avg} ppm`,
+      subtitle: 'carbon in the atmosphere.',
+    },
+    {
+      title: 'Today, there are',
+      value: '423.68 ppm',
+      subtitle: 'carbon in the atmosphere.',
+    },
+    {
+      title: `That's an increase of`,
+      value: `${increase} ppm`,
+      subtitle: 'during your lifetime thus far.',
+    },
+    {
+      title: 'These levels are speeding up. \n\n As a comparison, carbon only increased',
+      value: `${trend} ppm`,
+      subtitle: `during the ${difference} years before your birth.`,
+    },
+  ];
+
   return (
     <>
-  <Grid container spacing={2}>
-  <Grid item xs={3}>
-    <Box
-      sx={{
-        boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
-        borderRadius: '4px',
-        padding: '15px',
-        height: '90%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
-      <Typography variant="h5" align="center">
-        In {month} {year}, there were
-      </Typography>
-      <Typography component="span" variant="h5" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }} align="center">
-        {avg} ppm
-      </Typography>
-      <Typography variant="h5" align="center">
-        carbon in the atmosphere.
-      </Typography>
-    </Box>
-  </Grid>
-
-  <Grid item xs={3}>
-    <Box
-      sx={{
-        boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
-        borderRadius: '4px',
-        padding: '15px',
-        height: '90%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
-      <Typography variant="h5" align="center">
-        Today, there are
-      </Typography>
-      <Typography variant="h5" align="center" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }}>
-        423.68 ppm
-      </Typography>
-      <Typography variant="h5" align="center">
-        carbon in the atmosphere.
-      </Typography>
-    </Box>
-  </Grid>
-
-
-  <Grid item xs={3}>
-    <Box
-      sx={{
-        boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
-        borderRadius: '4px',
-        padding: '15px',
-        height: '90%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
-      <Typography variant="h5" align="center">
-        That's an increase of
-      </Typography>
-      <Typography component="span" variant="h5" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }} align="center">
-        {increase} ppm
-      </Typography>
-      <Typography variant="h5" align="center">
-        during your lifetime thus far.
-      </Typography>
-    </Box>
-  </Grid>
-
-
-  <Grid item xs={3}>
-    <Box
-      sx={{
-        boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
-        borderRadius: '4px',
-        padding: '15px',
-        height: '90%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
-      <Typography variant="h5" align="center">
-        These levels are speeding up. <br /><br /> As a comparison, carbon only increased
-      </Typography>
-      <Typography variant="h5" align="center">
-        <Typography component="span" variant="h5" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }} align="center">
-          {trend} ppm
-        </Typography>
-      </Typography>
-      <Typography variant="h5" align="center">
-        during the {difference} years <b>before</b> your birth.
-      </Typography>
-    </Box>
-  </Grid>
-</Grid>
-</>
-  )
+      <Grid container spacing={2}>
+        {trail.map((props, index) => (
+          <Grid item xs={3} key={index}>
+            <animated.div
+              style={{
+                ...props,
+                boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+                borderRadius: '4px',
+                padding: '15px',
+                height: '90%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}
+            >
+              <Typography variant="h5" align="center">
+                {cardData[index].title}
+              </Typography>
+              <Typography component="span" variant="h5" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }} align="center">
+                {cardData[index].value}
+              </Typography>
+              <Typography variant="h5" align="center">
+                {cardData[index].subtitle}
+              </Typography>
+            </animated.div>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default ResultsCards;
+
+
+
+
+
+// import React from 'react';
+// import { Typography, Box, Grid } from '@mui/material';
+
+// const ResultsCards = ({ month, year, avg, increase, difference, trend }) => {
+//   return (
+//     <>
+//       <Grid container spacing={2}>
+//         <Grid item xs={3}>
+//           <Box
+//             sx={{
+//               boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+//               borderRadius: '4px',
+//               padding: '15px',
+//               height: '90%',
+//               display: 'flex',
+//               flexDirection: 'column',
+//               justifyContent: 'center',
+//             }}
+//           >
+//             <Typography variant="h5" align="center">
+//               In {month} {year}, there were
+//             </Typography>
+//             <Typography component="span" variant="h5" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }} align="center">
+//               {avg} ppm
+//             </Typography>
+//             <Typography variant="h5" align="center">
+//               carbon in the atmosphere.
+//             </Typography>
+//           </Box>
+//         </Grid>
+
+//         <Grid item xs={3}>
+//           <Box
+//             sx={{
+//               boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+//               borderRadius: '4px',
+//               padding: '15px',
+//               height: '90%',
+//               display: 'flex',
+//               flexDirection: 'column',
+//               justifyContent: 'center',
+//             }}
+//           >
+//             <Typography variant="h5" align="center">
+//               Today, there are
+//             </Typography>
+//             <Typography variant="h5" align="center" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }}>
+//               423.68 ppm
+//             </Typography>
+//             <Typography variant="h5" align="center">
+//               carbon in the atmosphere.
+//             </Typography>
+//           </Box>
+//         </Grid>
+
+
+//         <Grid item xs={3}>
+//           <Box
+//             sx={{
+//               boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+//               borderRadius: '4px',
+//               padding: '15px',
+//               height: '90%',
+//               display: 'flex',
+//               flexDirection: 'column',
+//               justifyContent: 'center',
+//             }}
+//           >
+//             <Typography variant="h5" align="center">
+//               That's an increase of
+//             </Typography>
+//             <Typography component="span" variant="h5" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }} align="center">
+//               {increase} ppm
+//             </Typography>
+//             <Typography variant="h5" align="center">
+//               during your lifetime thus far.
+//             </Typography>
+//           </Box>
+//         </Grid>
+
+
+//         <Grid item xs={3}>
+//           <Box
+//             sx={{
+//               boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+//               borderRadius: '4px',
+//               padding: '15px',
+//               height: '90%',
+//               display: 'flex',
+//               flexDirection: 'column',
+//               justifyContent: 'center',
+//             }}
+//           >
+//             <Typography variant="h5" align="center">
+//               These levels are speeding up. <br /><br /> As a comparison, carbon only increased
+//             </Typography>
+//             <Typography variant="h5" align="center">
+//               <Typography component="span" variant="h5" sx={{ fontWeight: 'bold', fontSize: '2.5rem' }} align="center">
+//                 {trend} ppm
+//               </Typography>
+//             </Typography>
+//             <Typography variant="h5" align="center">
+//               during the {difference} years <b>before</b> your birth.
+//             </Typography>
+//           </Box>
+//         </Grid>
+//       </Grid>
+//     </>
+//   )
+// };
+
+// export default ResultsCards;
